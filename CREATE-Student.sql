387c527af2dad37d06396f0eaee0cd8d70554770
Vallarasu Sambathkumar <1884780+VallarasuS@users.noreply.github.com>
2026-02-05 11:14:12 +0530

    Fix phone_number data type from 'int' to 'INT'

--- CREATE-Student.sql
+++ CREATE-Student.sql
@@ -1,12 +1,12 @@
 CREATE TABLE students (
-    id          INT,
-    first_name  VARCHAR(100),
-    last_name   VARCHAR(100),
-    age         INT,
-    roll_number INT,
-    address     VARCHAR(250),
-    phone_number int,
-    language    INT,
-    science     INT,
-    maths       INT
+    id              INT,
+    first_name      VARCHAR(100),
+    last_name       VARCHAR(100),
+    age             INT,
+    roll_number     INT,
+    address         VARCHAR(250),
+    phone_number    INT,
+    language        INT,
+    science         INT,
+    maths           INT
 );
