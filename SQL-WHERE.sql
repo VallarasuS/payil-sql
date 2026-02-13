015013a862721ed48dae34e51736a9fd3d522a21
Vallarasu Sambathkumar <vals@live.in>
2026-02-13 21:17:13 +0530

    Update SQL-WHERE.sql

--- SQL-WHERE.sql
+++ SQL-WHERE.sql
@@ -6,12 +6,12 @@
 --          -----------------------------
 --          | Command     | Description |
 --          |-------------|-------------|
---          | =           |             |
---          | <           |             |
---          | >           |             |
---          | >=          |             |
---          | <=          |             |
---          | <>          |             |
+--          | =           | eq          |
+--          | <           | lt          |
+--          | >           | gt          |
+--          | >=          | gt or eq    |
+--          | <=          | lt or eq    |
+--          | <>          | not eq      |
 --          | LIKE        |             |
 --          | IS NULL     |             |
 --          | IS NOT NULL |             |
@@ -28,8 +28,56 @@
 --     condition / expression;
 
 ------------------------------------------------------------------------
+-- select all students who are from chennai
 
 SELECT * FROM 
     student 
+WHERE                           -- WHERE clause allows filtering
+    address = 'Chennai';        -- filter expression
+
+------------------------------------------------------------------------
+-- select a student with specific id
+
+SELECT * FROM 
+    student 
+WHERE                           
+    id = 100;               -- eq
+
+------------------------------------------------------------------------
+-- select students whose age is more than 17
+
+SELECT * FROM 
+    student 
+WHERE                           
+    age > 17;               -- gt
+
+------------------------------------------------------------------------
+-- select students whose last name is null (un-set)
+
+SELECT * FROM 
+    student 
+WHERE                           
+    last_name IS NULL;       -- is null
+------------------------------------------------------------------------
+-- select students who are from Chennai or Madurai
+
+SELECT * FROM 
+	student
 WHERE 
-    address = 'Chennai';
+	address IN ('Chennai', 'Madurai');
+------------------------------------------------------------------------ 
+-- select all students aged between 17 and 19
+
+SELECT * FROM 
+	student
+WHERE 
+	age BETWEEN 17 AND 19;
+------------------------------------------------------------------------ 
+-- select students whose names end with 'an'
+
+SELECT * FROM 
+	student
+WHERE 
+	first_name LIKE '%an'
+
+------------------------------------------------------------------------ 
