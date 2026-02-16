9d413b4dc4175b0dfb0abd6090294e8f743e567b
Vallarasu Sambathkumar <vals@live.in>
2026-02-16 09:43:39 +0530

    Sales Data

---
+++ SQL-AGG.sql
@@ -0,0 +1,16 @@
+------------------------------------------------------------------------
+--                          Aggregate Functions
+------------------------------------------------------------------------
+
+-- Aggregate functions compute a single result from a set of input values.
+
+--          ---------------------------------------------
+--          | Command | Description                     |
+--          |---------|---------------------------------|
+--          | AVG     | Computes average                |
+--          | COUNT   | Counts number of non-null rows  |
+--          | MIN     | Computes min of non-null values |
+--          | MAX     | Computes max of non-null values |
+--          | SUM     | Computes sum of non-null values |
+--          ---------------------------------------------
+
---
+++ SQL-ORDER-BY.sql
@@ -0,0 +1,13 @@
+------------------------------------------------------------------------
+--                          ORDER BY 
+------------------------------------------------------------------------
+
+-- Aggregate functions compute a single result from a set of input values.
+
+--          -----------------------------------------
+--          | Command  | Description                |
+--          |----------|----------------------------|
+--          | ORDER BY | Order rows by column value |
+--          | ASC      | A -> Z, low to high        |
+--          | DESC     | Z -> A, high to low        |
+--          -----------------------------------------
