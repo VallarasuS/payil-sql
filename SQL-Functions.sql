462c86e277618642cb3d54149fd3d2c5b91c5dad
Vallarasu Sambathkumar <vals@live.in>
2026-03-02 11:04:54 +0530

    Views and Functions

---
+++ SQL-Functions.sql
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------
+--                      SQL FUNCTIONS
+------------------------------------------------------------------------
+-- SYNTAX
+
+--  CREATE FUNCTION <function-name> (params type) RETURNS type
+--  LANGUAGE SQL
+--  AS $$
+--        <query>
+--  $$
+
+-- EXAMPLE 
+-- A function to increment input parameter by 1
+-- 5    -> 5  + 1   -> 6
+-- 10   -> 10 + 1   -> 11
+
+--  CREATE FUNCTION increment (a INT) RETURNS INT
+--  LANGUAGE SQL
+--  AS $$
+--      SELECT a + 1;
+--  $$
+
+--  SELECT INCREMENT(10);
+
+------------------------------------------------------------------------
+
+-- AGGREGATE FUNCTIONS
+-- SUM, MAX, COUNT, MIN, AVG
+-- SUM(price)
+
+ CREATE FUNCTION increment (a INT) RETURNS INT
+ LANGUAGE SQL
+ AS $$
+     SELECT a + 1;
+ $$
+
+ SELECT INCREMENT(10);
+
+------------------------------------------------------------------------
+
+CREATE FUNCTION student_by_city (city varchar(50)) RETURNS TABLE (id INT, first_name VARCHAR(200))
+LANGUAGE SQL
+AS $$
+    SELECT 
+        id, first_name
+    FROM 
+        student
+    WHERE
+        address = city;
+$$
+
+SELECT * FROM student_by_city('Chennai')
+SELECT * FROM student_by_city('Madurai')
+SELECT * FROM student_by_city('Trichy')
+
+------------------------------------------------------------------------
---
+++ SQL-VIEW.sql
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------
+--                      SQL VIEWS
+------------------------------------------------------------------------
+-- Save 'join' or any 'complex query' as a table like object called 'View'
+
+------------------------------------------------------------------------
+-- DEFINITION:
+-- View is a virtual table that represents a subset of data from 
+-- one or more base tables
+------------------------------------------------------------------------
+
+
+-- CREATE VIEW <view-name> 
+-- AS
+-- <QUERY>
+
+-- Example:
+
+-- CREATE VIEW chennai_student AS
+--      SELECT 
+-- 	    * 
+--      FROM 
+-- 	        student
+--      WHERE
+-- 	        address = 'Chennai'
+
+------------------------------------------------------------------------
+
+
+CREATE VIEW customer_orders AS
+	SELECT
+		c.id, c.fname, c.lname, o.id as "order_id", o.item, o.price, o.quantity
+	FROM 
+		customers c
+	JOIN
+		orders o
+	ON
+		o.customer_id = c.id;
+
+------------------------------------------------------------------------
+
+SELECT 
+	* 
+FROM
+	customer_orders
+WHERE 
+	fname = 'Emma';
+
+------------------------------------------------------------------------
